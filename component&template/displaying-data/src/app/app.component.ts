dfea9bf6934103b3821cf6d78946e6ee217a6f64
Ali_Alkinani <aalkinan@myune.edu.au>
2019-06-11 20:08:33 +1000

    reinitialize Hero component

--- component&template/displaying-data/src/app/app.component.ts
+++ component&template/displaying-data/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1> {{title}} </h1>
+                <h3>My favorite Hero : {{myHero}}</h3>` ,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'displaying-data';
+  myHero = 'Ali Alkinani' ;
 }
